d7bc7f18e3274c578827f4310a46f67b4ff64c9c
Joerg Schmuecker <j.schmuec@mongodb.com>
2023-08-16 15:06:07 +0200

    updated README to explain how it works.

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((clojure-mode
+  (cider-test-default-exclude-selectors . ("pending"))))
--- test/ct/main_test.clj
+++ test/ct/main_test.clj
@@ -1,16 +1,16 @@
 (ns ct.main-test
-(:require [clojure.test :refer [deftest testing is] :as ct]
-          [clojure.spec.alpha :as s]
-          [clojure.test.check :as tc]
-          [clojure.test.check.generators :as tcg]
-          [clojure.test.check.clojure-test :refer [defspec]]
-          [clojure.test.check.properties :as tcp]
-          [com.gfredericks.test.chuck.clojure-test :refer [for-all checking]]
-          [clojure.spec.gen.alpha :as gen]
-          [clojure.spec.test.alpha :as st]
-          [com.gfredericks.test.chuck :as chuck]
-          [ct.acct :as acct]
-          [ct.epoch :as epoch]
+  (:require [clojure.test :refer [deftest testing is] :as ct]
+            [clojure.spec.alpha :as s]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as tcg]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as tcp]
+            [com.gfredericks.test.chuck.clojure-test :refer [for-all checking]]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.test.alpha :as st]
+            [com.gfredericks.test.chuck :as chuck]
+            [ct.acct :as acct]
+            [ct.epoch :as epoch]
 ))
 
 (defn get-latest-epoch
