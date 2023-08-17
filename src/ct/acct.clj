c00bcc9588708d770f47cf5b05a3ee3f2193f939
Joerg Schmuecker <j.schmuec@mongodb.com>
2023-08-17 08:20:47 +0200

    implementing the new logic on accts

--- src/ct/acct.clj
+++ src/ct/acct.clj
@@ -9,53 +9,8 @@
 
 (s/def ::acct map?)
 
-(do
-  (defn available-amt
-    "calculates the available balance on an account, i.e. the net of balance and reservations"
-    [acct-doc]
-    (reduce + (:value acct-doc) 
-            (-> acct-doc
-                :pending
-                vals
-                )))
-
-  (s/fdef available-amt)
-
-  (def calculate-pending available-amt) ;; backwards compatibility
-  )
-
-(comment 
-  (calculate-pending 
-   {:value 100 :pending {:1 50}})
-)
-
-(do 
-  (defn create-epoch 
-    "creates epoch on a document by copying the latest epoch"
-    [doc epoch]
-    (cond
-      (get doc epoch) doc
-      (first doc) (assoc doc epoch (first (vals doc)))
-      :true (assoc {} epoch {})))
-  
-  (s/fdef create-epoch
-    :args (s/cat :doc map? :epoch any?))
-)
-
-(defn update-acct-if-pending
-  "udpates the balance and state of a pending tx-id. nop if tx-id is not pending "
-  [acct epoch tx-id]
-  (if-let [tx-value (get-in acct [epoch :pending tx-id])]
-    (-> acct 
-        (dissoc-in [epoch :pending tx-id])
-        (assoc-in [epoch :processed tx-id] tx-value)
-        (update-in [epoch :value] + tx-value))
-    acct)
-  )
-
-(defn update-acct-balance
-  [acct epoch tx-id]
-  (-> acct
-      (create-epoch epoch)
-      (update-acct-if-pending epoch tx-id)
-))
+(defn available-amt
+  [acct]
+  (let [latest-balance (or (-> acct :value vals last) 0)]
+    (reduce + latest-balance
+            (-> acct :pending vals))))
--- test/ct/acct_test.clj
+++ test/ct/acct_test.clj
@@ -3,23 +3,65 @@
 
             [ct.acct :as acct]))
 
-(deftest create-epoch-test
-  (testing "returns the doc unchanged if epoch exists"
-    (is (= {:e0 "xxx"}
-           (acct/create-epoch {:e0 "xxx"} :e0))))
-  (testing "returns the doc with the last epoch updated"
-    (is (= {:e0 "value from epoch 0"
-            :e1 "value from epoch 0"}
-           (acct/create-epoch {:e0 "value from epoch 0"} :e1)))))
+(def after-reservation
+  {:vs {"e-0" 0}
+   :pending {"tx-1" -100}})
 
-(deftest update-acct-if-pending-test
-  (testing "value is correct updated if tx is pending"
-    (is (=
-         {:e0 {:value 150 :processed {:tx1 50} :pending {}}}
-         (acct/update-acct-if-pending {:e0 {:value 100 :pending {:tx1 50}}} :e0 :tx1)
-         ))))
+(defn apply-if
+  [val p? f]
+  (if-let [var (p? val)]
+    (f val)
+    val))
 
-(deftest test-update-balance
-  (testing "that tx-1 is processed on acct"
-    (is (= {"e-1" {:value 200 :pending {} :processed {"tx-1" 100}}}
-           (acct/update-acct-balance  {"e-1" {:value 100 :pending {"tx-1" 100}}} "e-1" "tx-1")))))
+(defn apply-if-not
+  [val p? f]
+  (apply-if val (comp not p?) f))
+
+(deftest apply-if-test
+  (testing "applies transformation if predicates is true"
+    (is (= :result
+           (apply-if 1 odd? (fn [x] :result)))))
+  (testing "does not apply transformation if predicate is false"
+    (is (= 1
+           (apply-if 1 even? (fn [x] :result))))))
+
+(deftest apply-if-not-test
+  (testing "applies transformation if predicates is false"
+    (is (= :result
+           (apply-if-not 1 even? (fn [x] :result)))))
+  (testing "does not apply transformation if predicate is true"
+    (is (= 1
+           (apply-if-not 1 odd? (fn [x] :result))))))
+
+
+(defn consolidate
+  [acct epoch tx]
+  (let [last-balance (or (get-in acct [:vs epoch]) 0)]
+    (if-let [amt (get-in acct [:pending tx])]
+      (-> acct
+          (assoc-in [:value epoch] (+ last-balance amt))
+          (update :pending #(dissoc % tx)))
+      acct))
+  )
+
+(consolidate after-reservation "e1"  "tx-1")
+
+(deftest consolidate-test
+  (testing "consolidating a single transaction"
+    (is (= -100
+           (->
+               (consolidate after-reservation "e1" "tx-1")
+               (acct/available-amt)
+               )))))
+
+(deftest available-amt-test
+  (testing "available amt after an initial reservation"
+    (is 
+     (=
+      -100
+      (acct/available-amt after-reservation))))
+
+  (let [after-consolidation {:values {"e-0" 100}}]
+   )  
+
+)
