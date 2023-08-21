2ba1c22683bef3dc3ccf16ab2923b3d48a7e9812
Joerg Schmuecker <j.schmuec@mongodb.com>
2023-08-21 16:52:56 +0200

    removing test from main_test.clj that are just testing forwarding.

--- test/ct/main_test.clj
+++ test/ct/main_test.clj
@@ -14,32 +14,12 @@
             [ct.main :refer :all]
 ))
 
-
 (deftest connection-test
   (testing "that it creates a transaction file in the current epoch"
     (is (=
          {:epochs {"e-0" {:txfs {"my-file" 1}}}}
          (open-connection {:epochs {"e-0" {}}} "me" "my-file")))))
 
-(deftest account-state-test
-  (let [initial-db  {:accts {"payer" {:vs {"e0" 1000}}
-                             "payee" {:vs {"e0" 500}}}
-                     }
-        db (advise initial-db "tx-1" "payer" -100)]
-    (testing "after reservation"
-      (testing "balance is unchanged"
-        (is (= 1000
-               (-> db (get-balance "e0" "payer")))))
-      (testing "available amt is reduced"
-        (is (= 900
-               (-> db (get-available "payer"))))))
-    (let [db (advise db "tx-1" "payee" 100)]
-      (testing "after advise"
-        (testing "payee available amt is updated"
-          (is (= 600
-                 (get-available db "payee"))))
-        (testing "balance is unchanged"
-          (is (= 500
-                 (get-balance db "e0" "payee"))))))))
+
 
 
